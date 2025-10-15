6d06620091e601c9a36c3983dc3ff131b78a332f
stebecerra <128935494+stebecerra@users.noreply.github.com>
2025-10-15 00:42:14 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@
 
   // --- Configuration (Cloudflare Workers endpoints) ---
   var ENDPOINT_URL = 'https://online-worker.sbecerr7.workers.dev/';  // POST submissions here
-  var DATA_LOADER_URL = 'https://data-loader.sbecerr7.workers.dev/';     // GET sponsor/project data here
+  var DATA_LOADER_URL = 'https://online-data-loader.sbecerr7.workers.dev/';     // GET sponsor/project data here
   var STORAGE_KEY = 'sponsor_progress_v1';
   // set to 'online' for the online deployment, 'hybrid' or '' otherwise
   var DATA_SOURCE = 'online';
